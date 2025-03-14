35d26994843dd17f0f09932d3f366c916f8b0877
MIkaLaY-YukhNik <uhnikkola03@gmail.com>
2025-03-14 22:40:59 +0300

    Add readme 15

---
+++ src/main/java/com/example/currency_converter/repository/CurrencyRepository.java
@@ -0,0 +1,8 @@
+package com.example.currency_converter.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class CurrencyRepository {
+    // Этот класс пока пуст и не используется
+}
